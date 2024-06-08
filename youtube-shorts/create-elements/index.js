c494be86327bdb3624b7d5a015bcbfcbd873708b
SFERAS <rthyssen73@gmail.com>
2024-06-08 08:48:44 +0200

    table

---
+++ youtube-shorts/create-elements/index.js
@@ -0,0 +1,15 @@
+const listOfPersonal = {
+	name: "Ivan",
+	age: 41
+}
+console.log(listOfPersonal)
+// console.table(listOfPersonal)
+
+
+// -----------------------------------------------------------------
+document.body.style.background = "#007bff"
+document.body.style.textAlign = "center"
+document.body.style.fontFamily = "Roboto"
+document.body.style.color = "white"
+
+
---
+++ youtube-shorts/matches/index.js
@@ -0,0 +1,19 @@
+const birds = document.querySelectorAll("li");
+
+// for (const bird of birds) {
+// 	if (bird.matches(".predator")) {
+// 		bird.textContent = "Eagle is found";
+// 	}
+// }
+
+// -----------------------------------------------------------------
+document.body.style.background = "#007bff"
+document.body.style.display = "flex"
+document.body.style.fontFamily = "Roboto"
+document.body.style.color = "white"
+
+const wrapper = document.querySelector("#birds")
+wrapper.style.width = "fit-content"
+wrapper.style.margin = "0 auto"
+
+
