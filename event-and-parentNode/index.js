2c38eb3d9be1715f38dc7edf653858ad2ea92662
SFERAS <rthyssen73@gmail.com>
2024-06-10 00:24:01 +0200

    parenNode

---
+++ event-and-parentNode/index.js
@@ -0,0 +1,5 @@
+const childElement = document.getElementById('child');
+
+childElement.style.cursor = "pointer"
+childElement.parentNode.style.borderRadius = "10px"
+childElement.parentNode.style.backgroundColor = 'lightblue'
