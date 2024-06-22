daf5439746732a921b1d09f3271ea0868a4d0c8f
SFERAS <rthyssen73@gmail.com>
2024-06-22 08:08:01 +0200

    named modules done

--- Modules-1/module.js
+++ Modules-1/module.js
@@ -1,9 +1,22 @@
-import rotate from "./rotate.js"
+// default import
+// import rotate from "./rotate.js"
+// named import
+// import { stopRotate } from "./rotate.js"
+
+// combined default and named import
+// import rotate, { stopRotate, hide } from "./rotate.js"
+import rotate, * as rotateStuff from "./rotate.js"
+
+console.log(rotateStuff.test)
 
 document.querySelectorAll("h1").forEach(h1 => {
 	h1.addEventListener("click", event => {
-		rotate(h1)
+		if (h1.classList.contains("rotate")) {
+			stopRotate(h1)
+		} else {
+			rotate(h1)
+		}
 	})
 })
 
-console.log("Main JS")
+console.log("Main JS", rotateStuff)
--- Modules-1/rotate.js
+++ Modules-1/rotate.js
@@ -1,6 +1,25 @@
-// Default export method (inline)
-export default function rotate(element) {
-	element.classList.toggle("rotate")
+// Default export (inline)
+// export default function rotate(element) {
+// 	element.classList.toggle("rotate")
+// }
+
+// Default export (not inline)
+// export default rotate
+
+// Named export (inline)
+export function stopRotate(element) {
+	element.classList.remove("rotate")
 }
+
+// export const test = 7;
+// export default () => {console.log(2024)}
+const logNum = () => { console.log(2024) }
+export default logNum
+
+// Named export (not inline)
+function hide(element) {
+	element.hidden = true
+}
+export { hide }
 
 console.log("Rotate file")
