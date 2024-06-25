ffe888b251c02771191e87f99a8454c5eb1406ad
SFERAS <rthyssen73@gmail.com>
2024-06-25 22:39:10 +0200

    toggle 1 done

--- event-2-toggle/index.js
+++ event-2-toggle/index.js
@@ -2,23 +2,23 @@ const btn = document.querySelector("button");
 const mark = document.querySelector("#mark")
 const origin = mark.innerHTML
 
-// btn.addEventListener("click", () => {
-// if (mark.innerHTML === origin) {
-// 	mark.innerHTML = "Toggle is active!"
-// } else {
-// 	mark.innerHTML = origin
-// }
+btn.addEventListener("click", () => {
+	if (mark.innerHTML === origin) {
+		mark.innerHTML = "Toggle is active!"
+	} else {
+		mark.innerHTML = origin
+	}
 
-// btn.parentElement.classList.toggle("active")
-// btn.nextElementSibling.classList.add("fontChange")
-// 	// btn.nextElementSibling.classList.toggle("fontChange")
-// })
+	// btn.parentElement.classList.toggle("active")
+	// btn.nextElementSibling.classList.add("fontChange")
+	// 	// btn.nextElementSibling.classList.toggle("fontChange")
+	// })
 
-// --------------------- Bonus ------
+	// --------------------- Bonus ------
 
-// const wrapper = document.getElementById("wrapper")
+	// const wrapper = document.getElementById("wrapper")
 
-// wrapper.addEventListener("click", () => {
-// 	wrapper.lastElementChild.innerHTML = "Get practice and be master!"
-// })
+	// wrapper.addEventListener("click", () => {
+	// 	wrapper.lastElementChild.innerHTML = "Get practice and be master!"
+})
 
