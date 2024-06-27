6af8176b2f54709a97cbbe72c961671050dd477b
SFERAS <rthyssen73@gmail.com>
2024-06-27 11:49:55 +0200

    transpiling done

---
+++ npm/lib/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var _moment = _interopRequireDefault(require("moment"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+console.log((0, _moment["default"])().format("dd DD.MM"));
+// $ npm install moment
