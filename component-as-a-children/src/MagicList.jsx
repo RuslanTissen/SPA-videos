fd1786b950d921d3917126a247dec482eab17cc1
Ruslan Tissen <rthyssen73@gmail.com>
2024-09-04 20:44:34 +0200

    in progress

--- component-as-a-children/src/MagicList.jsx
+++ component-as-a-children/src/MagicList.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react"
+
 export default function MagicList(){
+    const [list, setList] = useState([])
+
+    function addToList() {
+        const newList = list.map(x => x)
+
+        newList.push("Hi")
+
+        setList(newList)
+    }
+
     return(
-        <ul>
-            <li>Hi From List</li>
+        <ul onClick={addToList}>
+            <h3>Magic List:</h3>
+            {list.map(item => <li>{item}</li>)}
         </ul>
     )
 }
