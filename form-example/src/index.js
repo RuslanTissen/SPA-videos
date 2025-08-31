f4f84ef319034339c0ba53f9a11280de3c6a7fe8
Ruslan Tissen <rthyssen73@gmail.com>
2025-08-31 10:37:27 +0200

    check all repo

--- form-example/src/index.js
+++ form-example/src/index.js
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 
+// Version with object state variable
 function App() {
-    const[name, setName] = useState("Old Muskus")
-    const[code, setCode] = useState("")
+    const [data, setData] = useState({name: "", code: ""})
 
     const handleNameChange = (e) => {
         setName(e.target.value)
@@ -13,10 +13,19 @@ function App() {
         setCode(e.target.value)
     }
 
-    console.log("Name: ", name, "Code: ",code);
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        // Here add yuor logic
+        // alert("Data ready to save")
+        // const data = {
+        //     name: name,
+        //     code: code
+        }
+        // Send the "data" to an API
+    }
 
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <input type='text' value={name} onChange={handleNameChange}></input>
             </div>
@@ -30,3 +39,42 @@ function App() {
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
+
+//Version with separate state varibles
+
+
+// function App() {
+//     const[name, setName] = useState("Steve Bezos")
+//     const[code, setCode] = useState("")
+// 
+//     const handleNameChange = (e) => {
+//         setName(e.target.value)
+//     }
+// 
+//     const handleCodeChange = (e) => {
+//         setCode(e.target.value)
+//     }
+// 
+//     const handleSubmit = (e) => {
+//         e.preventDefault()
+//         // Here add yuor logic
+//         alert("Data ready to save")
+//         const data = {
+//             name: name,
+//             code: code
+//         }
+//         // Send the "data" to an API
+//     }
+// 
+//     return(
+//         <form onSubmit={handleSubmit}>
+//             <div>
+//                 <input type='text' value={name} onChange={handleNameChange}></input>
+//             </div>
+//             <div>
+//                 <input type='text' value={code} onChange={handleCodeChange}></input>
+//             </div>
+//             <button>Add smth to database</button>
+//         </form>
+//     )
+// }
