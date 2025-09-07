1790ef47dd010a2ef0b1bef7bbbee816238e228e
Ruslan Tissen <rthyssen73@gmail.com>
2025-09-07 12:15:15 +0200

    7.9.25

--- form-example/src/index.js
+++ form-example/src/index.js
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 
-// Version with object state variable
 function App() {
-    const [data, setData] = useState({name: "", code: ""})
-    
+    const[name, setName] = useState("Steve Bez")
+    const[code, setCode] = useState("")
+
     const handleNameChange = (e) => {
         setName(e.target.value)
     }
@@ -15,7 +15,7 @@ function App() {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        // Here add yuor logic
+        // Here add your logic
         alert("Data ready to save")
         const data = {
             name: name,
@@ -25,7 +25,7 @@ function App() {
     }
 
     return(
-        <form onSubmit={handleSubmit}>
+        <form>
             <div>
                 <input type='text' value={name} onChange={handleNameChange}></input>
             </div>
@@ -57,7 +57,7 @@ root.render(<App />);
 // 
 //     const handleSubmit = (e) => {
 //         e.preventDefault()
-//         // Here add yuor logic
+//         // Here add your logic
 //         alert("Data ready to save")
 //         const data = {
 //             name: name,
@@ -73,6 +73,46 @@ root.render(<App />);
 //             </div>
 //             <div>
 //                 <input type='text' value={code} onChange={handleCodeChange}></input>
+//             </div>
+//             <button>Add smth to database</button>
+//         </form>
+//     )
+// }
+
+
+
+
+
+// Version with object state variable
+// function App() {
+//     const [data, setData] = useState({name: "", code: ""})
+    
+//     const handleNameChange = (e) => {
+//         setName(e.target.value)
+//     }
+
+//     const handleCodeChange = (e) => {
+//         setCode(e.target.value)
+//     }
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault()
+//         // Here add yuor logic
+//         alert("Data ready to save")
+//         const data = {
+//             name: name,
+//             code: code
+//         }
+//         // Send the "data" to an API
+//     }
+
+//     return(
+//         <form onSubmit={handleSubmit}>
+//             <div>
+//                 <input type='text' value={data.name} onChange={handleNameChange}></input>
+//             </div>
+//             <div>
+//                 <input type='text' value={data.code} onChange={handleCodeChange}></input>
 //             </div>
 //             <button>Add smth to database</button>
 //         </form>
