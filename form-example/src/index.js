684c24b1a78148e12fe8a6bcfb8819e6611fc5d1
Ruslan Tissen <rthyssen73@gmail.com>
2025-09-27 11:25:54 +0200

    input_field- in react finished

--- form-example/src/index.js
+++ form-example/src/index.js
@@ -1,6 +1,25 @@
 import { useState, useRef, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 
+function UncontrolledInput() {
+  const inputRef = useRef(null);
+
+  const showValue = () => {
+    alert(`Вы ввели: 
+      ${inputRef.current.value}`);
+  };
+
+  return (
+    <div>
+      <input ref={inputRef} placeholder="Введите текст" />
+      <button onClick={showValue}>Показать</button>
+    </div>
+  );
+}
+
+const root = createRoot(document.getElementById("root"));
+root.render(<UncontrolledInput />);
+
 // Version with a single state variable (OBJECT)
 function App() {
   const [data, setData] = useState({ login: "", psw: "" });
@@ -71,21 +90,3 @@ function RefTest() {
     </div>
   );
 }
-
-function UncontrolledInput() {
-  const inputRef = useRef(null);
-
-  const showValue = () => {
-    alert(`Вы ввели: ${inputRef.current.value}`);
-  };
-
-  return (
-    <div>
-      <input ref={inputRef} placeholder="Введите текст" />
-      <button onClick={showValue}>Показать</button>
-    </div>
-  );
-}
-
-const root = createRoot(document.getElementById("root"));
-root.render(<UncontrolledInput />);
