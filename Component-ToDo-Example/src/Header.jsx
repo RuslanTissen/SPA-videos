8f3654d2c8d8bd6a7ecd430553e7b6a93ed78830
Ruslan Tissen <rthyssen73@gmail.com>
2025-10-04 18:41:21 +0200

    4.10.25

--- Component-ToDo-Example/src/Header.jsx
+++ Component-ToDo-Example/src/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Header() {
-  return <h1>My Todo list</h1>;
+  return <h1>My Todo List</h1>;
 }
--- Component-ToDo-Example/src/TodoList.jsx
+++ Component-ToDo-Example/src/TodoList.jsx
@@ -1,5 +1,24 @@
-import React from "react";
+import { useState } from "react";
 
 export default function TodoList() {
-  return <p>Todo list</p>;
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("BB");
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={text} onChange={handleChange} />
+      <ul>
+        <li>Buy a tea</li>
+        <li>Go to a cinema</li>
+      </ul>
+    </form>
+  );
 }
