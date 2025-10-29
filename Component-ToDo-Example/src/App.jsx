56740e35d0a240fe5e91004fca55e0b158f5244e
Ruslan Tissen <rthyssen73@gmail.com>
2025-10-29 22:36:12 +0100

    29.10.25

--- Component-ToDo-Example/src/App.jsx
+++ Component-ToDo-Example/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header.jsx";
 import TodoList from "./TodoList.jsx";
+import ClickCounter from "./ClickCounter.jsx";
 
 export default function App() {
   return (
     <div>
-      <Header />
-      <TodoList />
+      {/* <Header /> */}
+      {/* <TodoList /> */}
+      <ClickCounter />
     </div>
   );
 }
---
+++ Component-ToDo-Example/src/ClickCounter.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function ClickCounter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Ты нажал {count} раз</p>
+      <button onClick={() => setCount(count + 1)}>Нажми меня</button>
+    </div>
+  );
+}
+
+export default ClickCounter;
