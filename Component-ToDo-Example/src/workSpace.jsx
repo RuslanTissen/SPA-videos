41a70eaf21cf36fafb686b9855884c4bb2ef4bfc
Ruslan Tissen <rthyssen73@gmail.com>
2025-11-11 19:30:11 +0100

    11.11.25

--- Component-ToDo-Example/src/workSpace.jsx
+++ Component-ToDo-Example/src/workSpace.jsx
@@ -1,8 +1,5 @@
-
-
-
-
-
-
-querySelector("body #wraper img");
-
+useEffect(() => {
+  fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((res) => res.json())
+    .then((data) => setTodos(data));
+}, []); // ← пустые скобки — наш щит от бесконечного рендера!
